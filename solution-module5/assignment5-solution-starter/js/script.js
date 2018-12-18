070dfcd7e5db0b62d04154e79ae73af4ef68dfd9
niazattari <niazattari2641@gmail.com>
2018-12-18 09:10:29 +0500

    my modified 5th assignment

--- solution-module5/assignment5-solution-starter/js/script.js
+++ solution-module5/assignment5-solution-starter/js/script.js
@@ -97,23 +97,13 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-
-      $ajaxUtils.sendGetRequest(
-        chooseRandomCategory,
-          function (chooseRandomCategory){
-            switchMenuToActive();
-            var chosenCategoryShortName = homeHtmlToInsertIntoMainPage (categories, 
-                                                                        homeHtml, 
-                                                                        chooseRandomCategory);
-             insertHtml("#main-content", chosenCategoryShortName);
-          }, 
-          false);
-
+     
+         
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-
+       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -127,7 +117,9 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-
+       homeHtml = insertProperty(homeHtml, "randomCategoryShortName", `'${chosenCategoryShortName}'`);
+      //
+      var homeHtmlToInsertIntoMainPage = insertHtml("#main-content", homeHtml);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
